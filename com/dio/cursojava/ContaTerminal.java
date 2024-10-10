e601f25d6d34f4f77a02d0119b7509565466e447
Samuel <samuel.goncalves@edu.unifil.br>
2024-10-10 20:43:08 -0300

    Primeiro commit

---
+++ com/dio/cursojava/ContaTerminal.java
@@ -0,0 +1,27 @@
+package com.dio.cursojava;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+
+        //Criação das variaveis e definição dos valores
+        System.out.println("Por favor, digite o numero da conta: ");
+        int numero = scan.nextInt();
+
+        System.out.println("Por favor, digite o numero da agencia: ");
+        String agencia = scan2.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scan2.nextLine();
+
+        System.out.println("Por favor, digite o saldo: ");
+        double saldo = scan.nextDouble();
+
+        //Imprime os valores digitados
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+    }
+}
+
